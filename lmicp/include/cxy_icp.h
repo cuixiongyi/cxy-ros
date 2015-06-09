7a453aaed954d62c346a31a9fbf9a95511888692
Xiongyi Cui <cuixiongyi2013@gmail.com>
2015-06-09 01:14:59 -0400

    finish cxy_icp setModel

--- lmicp/include/cxy_icp.h
+++ lmicp/include/cxy_icp.h
@@ -11,6 +11,15 @@
 
 #include "pcl/point_cloud.h"
 #include "pcl/point_types.h"
+#include <pcl/kdtree/kdtree_flann.h>
+#include "pcl_ros/transforms.h"
+
+#include "geometric_shapes/shape_operations.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "sensor_msgs/point_cloud_conversion.h"
+#include "visualization_msgs/Marker.h"
+#include "visualization_msgs/MarkerArray.h"
+#include "geometry_msgs/PoseStamped.h"
 
 #include "ros/ros.h"
 #include "cxy_transform.h"
@@ -29,13 +38,13 @@ namespace cxy
         typedef pcl::PointCloud<PointT>::ConstPtr    PointCloudConstPtr;
         typedef Eigen::Matrix< float, 3, 4> Matrix34f;
         typedef Eigen::Matrix< float, 3, 7> Matrix37f;
-        typedef Eigen::Matrix< float, E::Dynamic, 7> MatrixX7f;
+        typedef Eigen::Matrix< float, Eigen::Dynamic, 7> MatrixX7f;
         typedef Eigen::Matrix< float, 7, 7> Matrix7f;
         typedef Eigen::Matrix< float, 7, 1> Vector7f;
         typedef Eigen::Matrix< float, 6, 6> Matrix6f;
         typedef Eigen::Matrix< float, 6, 1> Vector6f;
 
-        typedef E::Matrix< float, 4, 4> Matrix44f;
+        typedef Eigen::Matrix< float, 4, 4> Matrix44f;
 
         class cxy_icp {
 
@@ -43,15 +52,17 @@ namespace cxy
         public:
             cxy_icp();
 
-            virtual float icp_run(pcl::PointCloud<pcl::PointXYZ>::Ptr inCloud, cxy_transform::Pose &outPose) = 0;
+            virtual float icp_run(pcl::PointCloud<pcl::PointXYZ>::Ptr inCloud, cxy_transform::Pose &outPose);
 
-            virtual float setModelCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr inCloud) = 0;
+            bool setModelCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr model);
 
+            //bool setModelCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr model);
 
         private:
             bool hasSetModelCloud_;
             ros::NodeHandle nh_, pnh_;
             ros::Publisher pub_model_, pub_model_pointcloud_, pub_data_pointcloud_, pub_result_;
+            pcl::KdTreeFLANN<PointT>::Ptr kdtreeptr_;
 
         protected:
             pcl::PointCloud<pcl::PointXYZ>::Ptr modelCloud_;
--- lmicp/src/cxy_icp.cpp
+++ lmicp/src/cxy_icp.cpp
@@ -11,17 +11,33 @@ namespace cxy {
     cxy_icp::cxy_icp() :  hasSetModelCloud_(false)
                         , pnh_("~")
     {
+        /*
         pnh_.param<int>("max_iterations", max_iterations_, 1000);
         pnh_.param<double>("transformation_epsilon", transformation_epsilon_, 1e-8);
         pnh_.param<double>("euclidean_fitness_epsilon", euclidean_fitness_epsilon_, 0.1);
         pnh_.param<double>("max_correspondence_dist", max_correspondence_dist_, 0.07); // 0.2
         max_correspondence_dist_square_ = max_correspondence_dist_*max_correspondence_dist_;
-
+        */
         pub_model_ = nh_.advertise<visualization_msgs::MarkerArray>("model", 5);
         pub_data_pointcloud_ = nh_.advertise<sensor_msgs::PointCloud2>("data_pointcloud", 5);
         pub_model_pointcloud_ = nh_.advertise<sensor_msgs::PointCloud2>("model_pointcloud", 5);
         pub_result_ = nh_.advertise<sensor_msgs::PointCloud2>("data_result", 5);
     }
 
+        bool cxy_icp::setModelCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr model)
+        {
+            if (nullptr == model || model->size() < 10)
+            {
+                return false;
+            }
+            hasSetModelCloud_ = true;
+            pcl::PointCloud<pcl::PointXYZ>::Ptr tmp = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> >(*model);
+            //translateToCenter(tmp);
+            modelCloud_ = tmp;
+            kdtreeptr_ = pcl::KdTreeFLANN<pcl::PointXYZ>::Ptr(new pcl::KdTreeFLANN<pcl::PointXYZ>);
+            kdtreeptr_->setInputCloud(modelCloud_);
+            return true;
+
+        }
     }
 }
